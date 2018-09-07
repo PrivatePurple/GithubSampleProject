4fd4c61b99c2dcab83dd9f8926726255ac5f1f25
Keenan <Keenanking555@gmail.com>
2018-09-07 13:29:03 -0600

    Added a time capsule.

--- src/controller/Runner.java
+++ src/controller/Runner.java
@@ -1,3 +1,5 @@
+// Dear Keenan of the future reading this, this is Keenan of  Friday, September 7, 2018. Here's your first Java Github file. I hope we've learned a lot by the time that you open this file back up. Consider this a time capsule, hehe.
+
 package controller;
 
 public class Runner
